fb3a1f4fa1c95295b22c0f96b7049dc198e928ed
7h3-3mp7y-m4n <emailtorash@gmail.com>
2025-07-19 02:45:37 +0530

    Added basic worker pool
    Signed-off-by: 7h3-3mp7y-m4n <emailtorash@gmail.com>

---
+++ 04-worker-pools/basic-worker-pool.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+const (
+	workerCount = 3
+	jobCount    = 5
+)
+
+func main() {
+	jobs := make(chan int)
+	var wg sync.WaitGroup
+
+	// Start worker goroutines
+	for i := 1; i <= workerCount; i++ {
+		go worker(i, jobs, &wg)
+	}
+
+	// Send jobs
+	for j := 0; j < jobCount; j++ {
+		wg.Add(1) // One wg.Add per job
+		jobs <- j
+	}
+
+	close(jobs) // Tell workers there are no more jobs
+	wg.Wait()   // Wait for all jobs to finish
+	fmt.Println("All workers done!")
+}
+
+func worker(id int, jobs <-chan int, wg *sync.WaitGroup) {
+	for job := range jobs {
+		fmt.Printf("Worker %d started job %d\n", id, job)
+		time.Sleep(time.Second)
+		fmt.Printf("Worker %d finished job %d\n", id, job)
+		wg.Done() // Signal this job is done
+	}
+}
